d61432e2c7c72c4efd914463071d73fa2e2a7cbe
Sushma <sushhpat@gmail.com>
2021-01-27 21:55:34 -0500

    updated class file

--- Roman_Kata.py
+++ Roman_Kata.py
@@ -1,4 +1,3 @@
-
 class NumeralsToRoman:
    
 
@@ -7,7 +6,30 @@ class NumeralsToRoman:
 
     
    def NumToRoman(self):
-        pass
+
+       Number = self.number
+       dict = { 1000:"M", 900:"CM", 500:"D", 400:"CD",
+                100:"C", 90:"XC", 50:"L", 40:"XL",
+                10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I" }
+
+       roman_number = ''
+       i = 0
+       while  Number > 0:
+           for k, v in dict.items():
+            while Number >= k:
+                roman_number += v
+                Number -= k
+
+           # for a in range(Number // val[i]):
+            #    roman_number += syb[i]
+            #    Number -= val[i]
+           # i += 1
+       return roman_number
+
+
+
+            
+
 
 
 
